87308ab19408ece79e3af7b3611f5480d630ef4d
miho-hoi <miho0829ko@gmail.com>
2023-10-23 16:09:55 +0900

    カラム名修正とデータベースの再リセット

--- db/migrate/20231021214337_devise_create_users.rb
+++ db/migrate/20231021214337_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :family_nam,         null: false
+      t.string :family_name,         null: false
       t.string :first_name,         null: false
       t.string :family_name_kan,    null: false
       t.string :first_name_kana,    null: false
