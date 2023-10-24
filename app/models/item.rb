0f6201197183fdde21615d437aa6aa21c965346f
miho-hoi <miho0829ko@gmail.com>
2023-10-24 15:07:31 +0900

    アクティブハッシュの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,20 @@
 class Item < ApplicationRecord
   validates :name,              presence: true
   validates :description,       presence: true
-  validates :category_id,       presence: true
-  validates :status_id,         presence: true
-  validates :postage_id,        presence: true
-  validates :region_id,         presence: true
-  validates :shipping_date_id,  presence: true
-  validates :price,             presence: true
+  validates :category_id,       presence: true, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :status_id,         presence: true, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :postage_id,        presence: true, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :region_id,         presence: true, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :shipping_date_id,  presence: true, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :price,             presence: true, numericality: { only_integer: true, greater_than: 300, less_than: 10000000 ,message: ""}
 
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipping_date
 end
