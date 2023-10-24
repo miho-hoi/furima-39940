d8d5c0e259873c872457339146059c06f7fa2c61
miho-hoi <miho0829ko@gmail.com>
2023-10-24 21:22:55 +0900

    ビュー画面とコントローラーの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   private
 
   def basic_auth
@@ -11,8 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,
-                                             :birthday])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,:birthday])
   end
 end
