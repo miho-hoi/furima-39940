3fea4cb3824fcf4baed72002c59dfca6d2ccbdd5
miho-hoi <miho0829ko@gmail.com>
2023-10-25 14:34:14 +0900

    テストコードの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
                                 numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 
   belongs_to :user
-  has_one_attached :image 
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
