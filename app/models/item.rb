c731cec8aa8407202c6b4a4dfaaa02a600e3a068
miho-hoi <miho0829ko@gmail.com>
2023-10-25 14:31:38 +0900

    整数のみのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,10 @@ class Item < ApplicationRecord
   validates :region_id,         numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_date_id,  numericality: { other_than: 1, message: "can't be blank" }
   validates :price,             presence: true,
-                                numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+                                numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :image 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
