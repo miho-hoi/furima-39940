2297cb025a100572d171b82396a7ff57a292c30b
miho-hoi <miho0829ko@gmail.com>
2023-10-26 21:12:28 +0900

    ログイン時のみ削除できるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: :edit
   def index
