b4a50c757d1ce0e6d082df5a7ead61debb5a5b3c
miho-hoi <miho0829ko@gmail.com>
2023-10-28 08:49:36 +0900

    モデルの作成とアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  has_one :purchase_record
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
