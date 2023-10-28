f3194f5573de3d35358abee1760a37b7a002f8f8
miho-hoi <miho0829ko@gmail.com>
2023-10-28 17:22:51 +0900

    ストロングパラメーターのモデル名の修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,7 +6,7 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    @purchase_address = PurchaseAddress.create(purchase_params)
+    @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       @purchase_address.save
       redirect_to root_path
@@ -18,7 +18,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:address).permit(:postal_code, :region_id, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:postal_code, :region_id, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_item
